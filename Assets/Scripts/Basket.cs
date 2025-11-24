d9b93512d8236640d7ee7f7528abeef2099f01cb
leofq <2203432@uad.ac.uk>
2025-11-24 16:16:35 +0000

    Update Basket.cs

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -17,7 +17,7 @@ public class Basket : MonoBehaviour
         m_TextComponent = GetComponentInChildren<TMP_Text>();
         m_TextComponent.text = GroupName;
         Vector3 pos = transform.position;
-        pos.y = 0.01f;
+        pos.y = -0.5f;
         transform.position = pos;
         GetComponent<Transform>().transform.Rotate(-90, 0, 0);
       
